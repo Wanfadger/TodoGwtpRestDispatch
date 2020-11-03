b50d512b482c376df5a8800d1a91895d09aff3a6
FADVAN8 <galiwango.fahad@gmail.com>
2020-11-03 17:20:32 +0300

    created basic todo ui

--- target/.generated/com/google/gwt/lang/com_00046wanfadger_00046todo_00046TodoGwtpRestDispatch__EntryMethodHolder.java
+++
@@ -1,6 +0,0 @@
-package com.google.gwt.lang;
-public class com_00046wanfadger_00046todo_00046TodoGwtpRestDispatch__EntryMethodHolder {
-  public static final void init() {
-    // to be synthesized later
-  }
-}
--- target/.generated/com/gwtplatform/dispatch/rest/client/DispatchRestEntryPointImpl.java
+++
@@ -1,6 +0,0 @@
-package com.gwtplatform.dispatch.rest.client;
-
-import com.gwtplatform.dispatch.rest.client.codegen.AbstractDispatchRestEntryPoint;
-
-public class DispatchRestEntryPointImpl extends AbstractDispatchRestEntryPoint {
-}
